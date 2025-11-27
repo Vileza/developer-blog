3a9a9f75190d727e95d57f90c9f63f9a3fb1eabc
Vileza <davidavid.dg@gmail.com>
2025-11-27 13:50:20 -0300

    Adiciona classe base para os controllers das requisições

---
+++ public_html/services/api-blog/app/Http/Controllers/BaseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\Api\ApiException;
+use App\Http\Resources\BaseResource;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+/**
+ * Classe base responsável pela definição dos métodos de consulta de modelos
+ * 
+ * @author David Guimarães
+ */
+abstract class BaseController extends Controller {
+
+  /**
+   * Método responsável por buscar um registro pelo ID
+   * @param  Model $obModel Modelo
+   * @return BaseResource
+   */
+  public function searchModel(Model $obModel): BaseResource {
+    throw new ApiException(406, 'Registro não encontrado');
+  }
+
+  /**
+   * Método responsável por buscar uma lista de registros
+   * @return ResourceCollection
+   */
+  public function searchList(): ResourceCollection {
+    throw new ApiException(406, 'Lista de registros não encontrada');
+  }
+}
