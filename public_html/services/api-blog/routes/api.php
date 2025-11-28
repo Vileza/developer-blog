c78d9083904ad77279ff1eaea028b93930fc65fc
Vileza <davidavid.dg@gmail.com>
2025-11-28 11:38:55 -0300

    Adicionada rotas de consultas dos posts sem a necessidade de autenticação

--- public_html/services/api-blog/routes/api.php
+++ public_html/services/api-blog/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Content\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', function (Request $request) {
-    return ['message' => 'Hello World'];
-});
+Route::get('/posts', [PostController::class, 'searchList']);
+Route::get('/posts/{post}', [PostController::class, 'searchModel']);
