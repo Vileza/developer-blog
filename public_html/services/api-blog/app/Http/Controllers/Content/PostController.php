a462e2e7d7a5df7421558c179aefdb9426356a4d
Vileza <davidavid.dg@gmail.com>
2025-11-30 18:21:45 -0300

    Altera o fluxo para a uma classe base de controller

--- public_html/services/api-blog/app/Http/Controllers/Content/PostController.php
+++ public_html/services/api-blog/app/Http/Controllers/Content/PostController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Content;
 
 use App\Exceptions\Api\ApiException;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 use App\Http\Resources\BaseResource;
 use App\Http\Resources\Content\PostResource;
 use App\Models\Content\Post;
+use App\Repository\Content\PostRepository;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 /**
@@ -14,15 +15,20 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
  * 
  * @author David Guimarães
  */
-class PostController extends Controller {
+class PostController extends BaseController {
     
   /**
    * Método responsável por buscar um registro pelo ID
    * @param  Post $post Modelo Post
    * @return PostResource
    */
-  public function searchModel(Post $post): BaseResource {
-    return new PostResource($post);  
+  public function searchModel($idPost): BaseResource {
+    if(!is_numeric($idPost)) throw new ApiException(400, 'Post não encontrado!');
+
+    $obPost = (new PostRepository)->getPostById($idPost);
+    if(!$obPost) throw new ApiException(404, 'Post não encontrado!');
+
+    return new PostResource($obPost);  
   }
 
   /**
