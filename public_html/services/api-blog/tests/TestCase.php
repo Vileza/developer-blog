6c5c2bcebdf22ba0402d6f7f4ec4a753a39f19f2
Vileza <davidavid.dg@gmail.com>
2025-11-30 18:23:52 -0300

    Configuração do ambiente incial do banco de dados de teste

--- public_html/services/api-blog/tests/TestCase.php
+++ public_html/services/api-blog/tests/TestCase.php
@@ -3,8 +3,30 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
-abstract class TestCase extends BaseTestCase
-{
-    //
+/**
+ * Classe base para os testes
+ * 
+ * @author David Guimarães
+ */
+abstract class TestCase extends BaseTestCase {
+
+  /**
+   * Método responsável por criar o banco de dados de teste
+   * @return void
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    //BUSCA O NOME DO BANCO DE DADOS NAS CONFIGURAÇÕES DE database.php
+    $databaseName = config('database.connections.mysql.database');
+    $connection   = DB::connection('mysql');
+
+    //CRIA O BANCO DE DADOS SE NÃO EXISTIR
+    $connection->getPdo()->exec("CREATE DATABASE IF NOT EXISTS `{$databaseName}`");
+
+    //USA O BANCO DE DADOS CRIADO
+    $connection->statement("USE `{$databaseName}`");
+  }
 }
