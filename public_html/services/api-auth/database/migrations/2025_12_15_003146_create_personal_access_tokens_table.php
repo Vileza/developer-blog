b55a357126ca8edd9a1b487e73b321b4532c0cd5
Vileza <davidavid.dg@gmail.com>
2026-01-01 18:56:35 -0300

    Alterações na migração para seguir com os campos necessários

--- public_html/services/api-auth/database/migrations/2025_12_15_003146_create_personal_access_tokens_table.php
+++ public_html/services/api-auth/database/migrations/2025_12_15_003146_create_personal_access_tokens_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\Auth\AuthenticationType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +12,7 @@ return new class extends Migration {
   public function up(): void {
     Schema::create('personal_access_tokens', function (Blueprint $table) {
       $table->id();
-      $table->morphs('tokenable');
-      $table->text('name');
+      $table->enum('type', AuthenticationType::getEnums())->default(AuthenticationType::READER);
       $table->string('token', 64)->unique();
       $table->timestamp('last_used_at')->nullable();
       $table->timestamp('expires_at')->nullable()->index();
