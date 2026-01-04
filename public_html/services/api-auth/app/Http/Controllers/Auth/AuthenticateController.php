b120dd9726a5a2bd58636c4917c7bd205d6c630d
Vileza <davidavid.dg@gmail.com>
2026-01-04 14:28:33 -0300

    Atualização do controller de autenticação

---
+++ public_html/services/api-auth/app/Http/Controllers/Auth/AuthenticateController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\DTOs\Auth\PayloadAuthTokenDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\PayloadFormRequest;
+use App\Service\Auth\AuthTokenGenerationService;
+
+class AuthenticateController extends Controller {
+
+  public function generateToken(PayloadFormRequest $request) {
+    $obPayloadAuthTokenDTO        = PayloadAuthTokenDTO::fromArray($request->validated());
+    $obAuthTokenGenerationService = new AuthTokenGenerationService($obPayloadAuthTokenDTO);
+    dd(json_encode($obPayloadAuthTokenDTO));
+    $token = $obAuthTokenGenerationService->generate();
+    dd($token);
+  }
+}
