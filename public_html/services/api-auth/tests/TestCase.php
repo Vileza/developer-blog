fb51bc1f2b9c01327cae88d7a455bc4a7a89f79c
Vileza <davidavid.dg@gmail.com>
2026-01-24 15:22:40 -0300

    Criação do banco de dados para realização dos testes de feature

--- public_html/services/api-auth/tests/TestCase.php
+++ public_html/services/api-auth/tests/TestCase.php
@@ -3,8 +3,27 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
-abstract class TestCase extends BaseTestCase
-{
-    //
+abstract class TestCase extends BaseTestCase {
+  
+  /**
+   * Método responsável por criar o banco de dados de teste
+   * @return void
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $driver = config('database.default');
+
+    //BUSCA O NOME DO BANCO DE DADOS NAS CONFIGURAÇÕES DE database.php
+    $databaseName = config('database.connections.'.$driver.'.database');
+    $connection   = DB::connection($driver);
+
+    //CRIA O BANCO DE DADOS SE NÃO EXISTIR
+    $connection->getPdo()->exec("CREATE DATABASE IF NOT EXISTS `{$databaseName}`");
+
+    //USA O BANCO DE DADOS CRIADO
+    $connection->statement("USE `{$databaseName}`");
+  }
 }
