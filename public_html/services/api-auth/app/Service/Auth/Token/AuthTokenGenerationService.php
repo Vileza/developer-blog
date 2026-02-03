5a31c85dbd0878f404df45ee522c4ab6463f4fa7
Vileza <davidavid.dg@gmail.com>
2026-02-03 17:36:58 -0300

    Geração do token JWT

--- public_html/services/api-auth/app/Service/Auth/Token/AuthTokenGenerationService.php
+++ public_html/services/api-auth/app/Service/Auth/Token/AuthTokenGenerationService.php
@@ -3,6 +3,7 @@
 namespace App\Service\Auth\Token;
 
 use App\DTOs\Auth\PayloadAuthTokenDTO;
+use Firebase\JWT\JWT;
 
 /**
  * Classe serviço responsável por gerar o token de autenticação
@@ -19,7 +20,7 @@ class AuthTokenGenerationService {
 
   /**
    * Método construtor
-   * @param  PayloadAuthTokenDTO $obPayloadAuthTokenDTO
+   * @param  PayloadAuthTokenDTO $obPayloadAuthTokenDTO Instância do DTO de payload de autenticação
    * @return void
    */
   public function __construct(PayloadAuthTokenDTO $obPayloadAuthTokenDTO) {
@@ -31,14 +32,23 @@ class AuthTokenGenerationService {
    * @return string
    */
   public function generate() {
-    return $this->encryptPayload();
+    $tokenJWT = $this->generateTokenJWT();
+    return $this->encrypt($tokenJWT);
+  }
+
+  /**
+   * Método responsável por gerar o token JWT
+   * @return string
+   */
+  private function generateTokenJWT(): string {
+    return JWT::encode($this->payloadAuthTokenDTO->asArray(), env('APP_KEY'), 'HS256');
   }
 
   /**
    * Método responsável por criptografar o payload de autenticação
    * @return string
    */
-  private function encryptPayload(): string {
-    return encrypt(json_encode($this->payloadAuthTokenDTO));
+  private function encrypt(string $tokenJWT): string {
+    return encrypt($tokenJWT);
   }
 }
